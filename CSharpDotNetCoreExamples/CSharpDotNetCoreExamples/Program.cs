ef7a585407d9a0d084932034756d6db9f5d31272
Won Kim <wkim1@lenovo.com>
2020-05-27 09:23:21 -0700

    Added F# and C#, side by side, examples.

---
+++ CSharpDotNetCoreExamples/CSharpDotNetCoreExamples/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CSharpDotNetCoreExamples
+{
+    public class Program
+    {
+        public static int Add(IEnumerable<int> numbers)
+        {
+            // The problem with C# is that numbers could be null, so usually
+            //      the code has to account for it.
+            if (numbers == null)
+                return 0;
+
+            // Imperative
+            var result = 0;
+            foreach (var num in numbers)
+            {
+                result += num;
+            }
+            return result;
+            // Linq. C# way of doing functional programming.
+            //return numbers?.Any() ?? false ? numbers.Aggregate((x, y) => x + y) : 0;
+            //return numbers?.Any() ?? false ? numbers.Sum() : 0;
+        }
+
+        public static void AddSample()
+        {
+            var result = 0;
+
+            // Array
+            var numbersArray = new[] { 10, 13, 1, 2, 4, 5 };
+            Console.WriteLine($"[{string.Join(',', numbersArray)}]");
+
+            result = Add(numbersArray);
+            Console.WriteLine(result);
+
+            // List
+            var numbersList = new List<int> { 10, 13, 1, 2, 4, 5 };
+            Console.WriteLine($"[{string.Join(',', numbersList)}]");
+
+            result = Add(numbersList);
+            Console.WriteLine(result);
+
+            // Singleton
+            numbersList = new List<int> {10};
+            Console.WriteLine($"[{string.Join(',', numbersList)}]");
+
+            result = Add(numbersList);
+            Console.WriteLine(result);
+
+            // Empty
+            numbersList = new List<int>();
+            Console.WriteLine($"[{string.Join(',', numbersList)}]");
+
+            result = Add(numbersList);
+            Console.WriteLine(result);
+        }
+
+        public static List<int> GenerateFibonacciNumbers(int numValues)
+        {
+            if (numValues == 0)
+                return new List<int>();
+            if (numValues == 1)
+                return new List<int> {1};
+
+            var fibonnaciSeq = new List<int> { 1, 1 };
+            for (var i = 2; i < numValues; i++)
+            {
+                var prior = fibonnaciSeq[fibonnaciSeq.Count - 2];
+                var last = fibonnaciSeq[fibonnaciSeq.Count - 1];
+                fibonnaciSeq.Add(prior + last);
+            }
+            return fibonnaciSeq;
+        }
+
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            AddSample();
+            var fibonacciSeq = GenerateFibonacciNumbers(10);
+            Console.WriteLine($"[{string.Join(',', fibonacciSeq)}]");
+        }
+    }
+}
---
+++ FSharpDotNetCoreExamples/FSharpDotNetCoreExamples/Program.fs
@@ -0,0 +1,85 @@
+﻿// Learn more about F# at http://fsharp.org
+
+open System
+open System.Collections.Generic
+
+
+// C-like function declarations. Functions must be declared ABOVE usage.
+// Using explicit typing. F# does not require explicit typing.
+//let add (numbers) =
+let add (numbers : int seq) : int =
+    Seq.fold (fun state item -> state + item) 0 numbers // func, seed, seq
+    // reduce throws an exception if seq is empty.
+    //if Seq.isEmpty numbers then 0
+    //else numbers |> Seq.reduce (fun x y -> x + y)
+    // sum is a special case of reduce.
+    //else numbers |> Seq.sum
+
+
+let addImmutableSample =
+    // Array
+    let numbers = [|10; 13; 1; 2; 4; 5|]
+    printfn "%A" numbers
+
+    let result = add(numbers)
+    printfn "%i" result
+
+    // List
+    let numbers = [10; 13; 1; 2; 4; 5]
+    printfn "%A" numbers
+
+    let result = add(numbers)
+    printfn "%i" result
+
+    // Singleton
+    let numbers = Seq.singleton 10
+    printfn "%A" numbers
+
+    let result = add(numbers)
+    printfn "%i" result
+
+    // Empty
+    let numbers = []
+    printfn "%A" numbers
+
+    let result = add(numbers)
+    printfn "%i" result
+
+
+let addMutableSample =
+    // FYI, F# doesn't help you add open (using) statements.
+    let numbers = new List<int>()
+    numbers.AddRange([10; 13; 1; 2; 4; 5])
+    printfn "%s" (String.Join(',', numbers)) // Very C#
+    let result = add(numbers)
+    printfn "%i" result
+
+    // Generally, it doesn't make sense to use .NET generic lists in F#.
+    //      Simply join lists.
+    let mutable numbers = [10; 13; 1; 2; 4; 5]
+    numbers <- 6 :: numbers
+    numbers <- numbers @ [3; 5; 7; 9; 11]
+    printfn "%A" numbers
+
+    let result = add(numbers)
+    printfn "%i" result
+
+// Produces an infinite list of Fibonnaci numbers.
+let generateFibonacciNumbers : int seq =
+    (1, 1)
+    |> Seq.unfold (fun state ->
+        let next = fst state + snd state
+        Some(next, (snd state, next)))
+
+[<EntryPoint>]
+let main argv =
+    printfn "Hello World from F#!"
+
+    addImmutableSample
+    addMutableSample
+
+    printfn "%A" generateFibonacciNumbers
+    let fibonnaciSeq = generateFibonacciNumbers |> Seq.take 10 |> Seq.toList
+    printfn "%A" fibonnaciSeq
+
+    0 // return an integer exit code
